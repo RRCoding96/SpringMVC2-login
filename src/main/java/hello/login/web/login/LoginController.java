648bf9c90cc0217b58a75a58e37be8356bb1296c
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-04-06 22:26:26 +0900

    feat: 로그인 처리하기 - 쿠키 사용

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -40,9 +41,22 @@ public class LoginController {
 
         //로그인 성공 처리
 
-        return "redirect:/";
-
         //쿠키에 시간 정보를 주지 않으면 세션 쿠기(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
